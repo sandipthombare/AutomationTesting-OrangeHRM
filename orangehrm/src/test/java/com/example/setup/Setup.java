c1501f71b73239dfbcdc501e8db69ea7fe2e0a10
Sandy <sandipthombare108@gmail.com>
2024-03-30 17:03:10 +0530

    Updated Dashboard and EmployeeInfoPage

--- orangehrm/src/test/java/com/example/setup/Setup.java
+++ orangehrm/src/test/java/com/example/setup/Setup.java
@@ -6,26 +6,28 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 
+import com.example.pages.DashboardPage;
+
 import java.time.Duration;
-import java.util.concurrent.TimeUnit;
 
 public class Setup {
     public WebDriver driver;
 
-    @SuppressWarnings("deprecation")
     @BeforeTest
     public void setup(){
         WebDriverManager.edgedriver().setup();
 
         driver = new EdgeDriver();
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30).toMillis(), TimeUnit.MILLISECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         
     }
 
     @AfterTest
     public void closeDriver(){
+         DashboardPage dashboardPage = new DashboardPage(driver);
+        dashboardPage.doLogout();
         driver.close();
     }
 }
